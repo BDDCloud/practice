41f4aefed3c21008b1fdeb72af65eeac617e7263
Sean Bennett <Sean.Bennett@sjrb.ca>
2013-06-14 23:47:37 -0600

    make static todo list in node

--- angular-nodejs1/server.js
+++ angular-nodejs1/server.js
@@ -1,12 +1,25 @@
 var http = require("http");
-
+var todo =
+    [
+        { description: "read angular ebook", id: "1234-5678" },
+        { description: "add e2e", id: "5678-1234" }
+    ];
 http.createServer(function(request, response) {
-    response.writeHead(200,
-        {
-            "Content-Type" : "application/json"
-            ,"Access-Control-Allow-Origin" : "*"
-        });
-    //response.write("\")]}',\n\"[{ 'description':'read angular ebook', 'id': '1234-5678' }, { 'description':'add e2e tests', 'id': '1234-4321' }]");
-    response.write("[{ \"description\":\"read angular ebook\", \"id\": \"1234-5678\" }, { \"description\":\"add e2e tests\", \"id\": \"1234-4321\" }]");
-    response.end();
+
+    if(request.method.toLowerCase() == 'get')
+    {
+        response.writeHead(200,
+            {
+                "Content-Type" : "application/json"
+                ,"Access-Control-Allow-Origin" : "*"
+            });
+
+        response.write(JSON.stringify(todo));
+        response.end();
+        todo.push({ description: "read javascript good parts", id: "2343-1238"});
+    }
+    else if(request.method.toLowerCase() == 'post')
+    {
+
+    }
 }).listen(8123);
